1f46a37d2220454c4292267762a149ddac97217a
HungThinh <nhthinh.it@gmail.com>
2023-11-07 07:23:35 +0000

    Initialize component plugin

---
+++ src/Component.svelte
@@ -0,0 +1,15 @@
+<script>
+  import {createEventDispatcher, onMount} from "svelte"
+  const dispatch = createEventDispatcher();
+
+  /**
+   * Must be `trigger` value
+   * @type Function
+   */
+  export let trigger
+
+  onMount(() => {
+    dispatch(trigger());
+  });
+
+</script>
